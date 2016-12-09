a856bc220ed1a947d09ad9ae29cc1873c364bfc6
Sergey Ivanov <ivanovs-4@yandex.ru>
2016-12-09 14:38:39 +0500

    Extract ZClient.construct_from_about()

--- services/directory/client.py
+++ services/directory/client.py
@@ -33,7 +33,7 @@ class Directory(ZClient):
 
     def get_client_for(self, service):
         s_info = self._service_info(service)
-        return service.client(s_info)
+        return service.client.construct_from_about(s_info)
 
     def query_service(self, service, *args):
         asked_client = self.get_client_for(service)
--- z_arch.py
+++ z_arch.py
@@ -2,8 +2,12 @@ from transport import ReqRepMsgpackTransport
 
 
 class ZClient:
-    def __init__(self, info):
-        self._address = info[0]['rep_address']
+    def __init__(self, address):
+        self._address = address
+
+    @classmethod
+    def construct_from_about(cls, about):
+        return cls(about[0]['rep_address'])
 
     def query_raw(self, *args):
         tran = ReqRepMsgpackTransport(self._address)
